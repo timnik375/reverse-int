832c6a36721df970e4d54786ee026c840d1003a9
timnik375 <67319575+timnik375@users.noreply.github.com>
2021-03-16 00:43:36 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n >= 0) {
+        n = String(n).split('').reverse().join('')
+    } else {
+        n *= -1;
+        n = String(n).split('').reverse().join('')
+    }
+
+    return n  
 }
